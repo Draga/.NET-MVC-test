0fa006c47ce5bf849f9b88deca5ad4429072cd18
Stefano Chiodino <LouderTH@gmail.com>
2016-03-01 23:37:10 +0000

    Edit person view, model and action

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -20,12 +20,51 @@ namespace WebApplication1.Models
             return View(await db.People.ToListAsync());
         }
 
+        // GET: People/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Person person = await db.People.FindAsync(id);
+
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(EditPersonModel editPersonModel)
+        {
+            var person = db.People.First(p => editPersonModel.PersonId == p.PersonId);
+            if (ModelState.IsValid)
+            {
+                person.IsAuthorised = editPersonModel.IsAuthorised;
+                person.IsEnabled = editPersonModel.IsEnabled;
+                db.Entry(person).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
                 db.Dispose();
             }
+
             base.Dispose(disposing);
         }
     }
---
+++ WebApplication1/WebApplication1/Models/EditPersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EditPersonModel
+    {
+        public int PersonId { get; set; }
+
+        [DisplayName("Authorised")]
+        public bool IsAuthorised { get; set; }
+
+        [DisplayName("Enabled")]
+        public bool IsEnabled { get; set; }
+
+        public List<Colour> FavouriteColors { get; set; } 
+    }
+}
